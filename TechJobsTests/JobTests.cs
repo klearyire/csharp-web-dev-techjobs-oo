c40f6fa991f610a8c7cda0355bf9fb6306719a49
Kiaran <klearyire@gmail.com>
2021-10-20 16:50:42 -0400

    more tests

--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -6,19 +6,46 @@ namespace TechJobsTests
     [TestClass]
     public class JobTests
     {
-        [TestMethod]
-        public void EmptyTest()
+        Job test_job1;
+        Employer acme;
+        Location desert;
+        PositionType qa;
+        CoreCompetency persistence;
+
+        [TestInitialize]
+        public void CreateJobAndFields()
         {
-            Assert.AreEqual(10, 10, .001);
+            acme = new Employer("ACME");
+            desert = new Location("Desert");
+            qa = new PositionType("Quality control");
+            persistence = new CoreCompetency("Persistence");
+
+            test_job1 = new Job("Product tester", acme, desert, qa, persistence);
         }
 
         [TestMethod]
         public void TestSettingJobId()
         {
-            Job test_job1 = new Job();
-            Job test_job2 = new Job();
+            Job job1 = new Job();
+            Job job2 = new Job();
 
-            Assert.IsFalse(test_job1 == test_job2);
+            Assert.IsTrue(job2.Id - job1.Id == 1);
+        }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Assert.IsTrue(test_job1.Name == "Product tester");
+            Assert.IsTrue(test_job1.EmployerName.Value == "ACME");
+            Assert.IsTrue(test_job1.EmployerLocation.Value == "Desert");
+            Assert.IsTrue(test_job1.JobCoreCompetency.Value == "Quality control");
+            Assert.IsTrue(test_job1.JobType.Value == "Persistence");
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+
         }
     }
 }
